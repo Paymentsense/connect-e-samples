17fe16978612f2b0b5794f337a485f7a82a0313d
Dan Belden <dan@replicada.com>
2021-09-17 11:08:04 +0100

    [PHP] Add few more web tests

--- php/tests/WebTest.php
+++ php/tests/WebTest.php
@@ -11,12 +11,39 @@ class WebTest extends TestCase
         $this->assertEquals($expectedView->render(), $this->response->getContent());
     }
 
+    public function testPreAuth()
+    {
+        $this->get('/pre-auth');
+        $this->assertResponseOk();
+
+        $expectedView = view('pre-auth');
+        $this->assertEquals($expectedView->render(), $this->response->getContent());
+    }
+
+    public function testRefund()
+    {
+        $this->get('/refund');
+        $this->assertResponseOk();
+
+        $expectedView = view('refund');
+        $this->assertEquals($expectedView->render(), $this->response->getContent());
+    }
+
     public function testSale()
     {
         $this->get('/sale');
         $this->assertResponseOk();
 
         $expectedView = view('sale');
+        $this->assertEquals($expectedView->render(), $this->response->getContent());
+    }
+
+    public function testSubscription()
+    {
+        $this->get('/subscription');
+        $this->assertResponseOk();
+
+        $expectedView = view('subscription');
         $this->assertEquals($expectedView->render(), $this->response->getContent());
     }
 }
