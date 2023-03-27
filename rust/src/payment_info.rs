7ed7c22e61470f687016eeb08731aeedd9399c35
Max Alencar <maxdanielton@gmail.com>
2023-03-27 14:49:33 +0100

    fix tabulation

--- rust/src/payment_info.rs
+++ rust/src/payment_info.rs
@@ -4,17 +4,17 @@ use serde::{Serialize, Deserialize};
 #[serde(rename_all = "camelCase")]
 pub struct PaymentInfo {
 	auth_code         		:Option<String>,
-	billing_address         :Option<Address>,
+	billing_address			:Option<Address>,
 	expiry_date        		:String,
-	card_name             	:Option<String>,
+	card_name				:Option<String>,
 	card_number        		:String,
 	card_type        		:String,
-	cross_reference         :Option<String>,
-	message       			:Option<String>,
-	payment_method_id       :Option<String>,
-	status_code         	:i32,
-	transaction_date_time 	:Option<String>,
-	user_email_address      :Option<String>,
+	cross_reference			:Option<String>,
+	message					:Option<String>,
+	payment_method_id		:Option<String>,
+	status_code				:i32,
+	transaction_date_time	:Option<String>,
+	user_email_address		:Option<String>,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -25,7 +25,7 @@ struct Address {
 	address_3         		:Option<String>,
 	address_4         		:Option<String>,
 	city         			:Option<String>,
-	country_code         	:Option<String>,
+	country_code			:Option<String>,
 	state         			:Option<String>,
 	postcode         		:Option<String>,
 }
